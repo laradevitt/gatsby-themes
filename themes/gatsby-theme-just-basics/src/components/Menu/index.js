e2d44260d293ec83265b6fa7f7026592998beb36
laradevitt <ldevitt@othermachines.com>
2021-03-29 10:37:49 -0600

    Menu component improvements; edit package.json author.

--- themes/gatsby-theme-just-basics/src/components/Menu/index.js
+++ themes/gatsby-theme-just-basics/src/components/Menu/index.js
@@ -32,12 +32,15 @@ const Menu = ({ activePath, wrapperClass }) => (
     render={(data) => {
       const { edges } = data.allMenuItem;
       const menuTree = createDataTree(edges, activePath);
-      if (edges.length) {
+      if (edges.length > 0) {
         return (
           <nav className={wrapperClass}>
             {<MenuList activePath={activePath} items={menuTree} />}
           </nav>
         );
+      }
+      else {
+        return (null);
       }
     }}
   />
